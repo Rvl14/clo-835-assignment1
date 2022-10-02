2dcaeaa86b36638b58dc33824b5f81e7dd065bc8
Rvl14 <114627791+Rvl14@users.noreply.github.com>
2022-10-01 20:48:32 -0400

    Update variables.tf

--- terraform_code/dev/instances/variables.tf
+++ terraform_code/dev/instances/variables.tf
@@ -12,13 +12,13 @@ variable "instance_type" {
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "dev"
+  default     = "development"
   type        = string
   description = "Deployment Environment"
 }
 
 variable "ecr_repo" {
-  default     = ["app", "mysql"]
+  default     = ["software", "mysqldatabase"]
   type        = set(string)
   description = "Amazon ECR Repository Names"
 }
